1d525fd32ed9422ab717177f95bbe838903c3ef6
rafaelpoveronferreira <rafael.poveron.ferreira@gmail.com>
2023-01-06 12:06:50 -0300

    trying to fix

--- index.js
+++ index.js
@@ -12,6 +12,29 @@ app.use(helmet());
 const cors = require('cors')
 const corsOptions = require('./corsOptions')
 app.use(cors(corsOptions))
+const WHITELIST = [
+    '3.18.12.63',
+    '3.130.192.231',
+    '13.235.14.237',
+    '13.235.122.149',
+    '18.211.135.69',
+    '35.154.171.200',
+    '52.15.183.38',
+    '54.88.130.119',
+    '54.88.130.237',
+    '54.187.174.169',
+    '54.187.205.235',
+    '54.187.216.72',
+    'https://rafaelpoveronferreira.github.io/NIEHS-ecommerce-react',
+    'http://localhost:5173',
+    'http://localhost:5174',
+    'http://localhost:4173',
+    'http://localhost:4174']
+
+app.use((req, res) => {
+    res.header("Access-Control-Allow-Origin", WHITELIST.some(e=>e==req.header.origin)?req.header.origin:'')
+})
+
 
 const mongoose = require('mongoose');
 
